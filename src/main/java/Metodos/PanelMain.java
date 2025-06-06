d3967372ba571107db859b388383faf5e4549170
Aaron Archaga <Aaron Archaga@Laptop_Archaga>
2025-06-06 09:26:21 -0600

    prueba de commit

---
+++ src/main/java/Metodos/PanelMain.java
@@ -0,0 +1,274 @@
+package Metodos;
+
+public class PanelMain extends javax.swing.JFrame {
+    
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(PanelMain.class.getName());
+
+   
+    public PanelMain() {
+        initComponents();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        Titulo = new javax.swing.JLabel();
+        btn2 = new javax.swing.JButton();
+        btn1 = new javax.swing.JButton();
+        btn3 = new javax.swing.JButton();
+        btn4 = new javax.swing.JButton();
+        btn5 = new javax.swing.JButton();
+        btn8 = new javax.swing.JButton();
+        btn7 = new javax.swing.JButton();
+        btn6 = new javax.swing.JButton();
+        btn9 = new javax.swing.JButton();
+        btn12 = new javax.swing.JButton();
+        btn11 = new javax.swing.JButton();
+        btn10 = new javax.swing.JButton();
+        btn13 = new javax.swing.JButton();
+        btn16 = new javax.swing.JButton();
+        btn15 = new javax.swing.JButton();
+        btn14 = new javax.swing.JButton();
+        jButton17 = new javax.swing.JButton();
+
+        jLabel1.setText("jLabel1");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        Titulo.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        Titulo.setText("Memorama");
+
+        btn2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
+        btn2.setMaximumSize(new java.awt.Dimension(60, 60));
+        btn2.setMinimumSize(new java.awt.Dimension(60, 60));
+        btn2.setPreferredSize(new java.awt.Dimension(60, 60));
+
+        btn1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
+        btn1.setMaximumSize(new java.awt.Dimension(60, 60));
+        btn1.setMinimumSize(new java.awt.Dimension(60, 60));
+        btn1.setPreferredSize(new java.awt.Dimension(60, 60));
+
+        btn3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
+        btn3.setMaximumSize(new java.awt.Dimension(60, 60));
+        btn3.setMinimumSize(new java.awt.Dimension(60, 60));
+        btn3.setPreferredSize(new java.awt.Dimension(60, 60));
+        btn3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn3ActionPerformed(evt);
+            }
+        });
+
+        btn4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
+        btn4.setMaximumSize(new java.awt.Dimension(60, 60));
+        btn4.setMinimumSize(new java.awt.Dimension(60, 60));
+        btn4.setPreferredSize(new java.awt.Dimension(60, 60));
+
+        btn5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
+        btn5.setMaximumSize(new java.awt.Dimension(60, 60));
+        btn5.setMinimumSize(new java.awt.Dimension(60, 60));
+        btn5.setPreferredSize(new java.awt.Dimension(60, 60));
+
+        btn8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
+        btn8.setMaximumSize(new java.awt.Dimension(60, 60));
+        btn8.setMinimumSize(new java.awt.Dimension(60, 60));
+        btn8.setPreferredSize(new java.awt.Dimension(60, 60));
+
+        btn7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
+        btn7.setMaximumSize(new java.awt.Dimension(60, 60));
+        btn7.setMinimumSize(new java.awt.Dimension(60, 60));
+        btn7.setPreferredSize(new java.awt.Dimension(60, 60));
+
+        btn6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
+        btn6.setMaximumSize(new java.awt.Dimension(60, 60));
+        btn6.setMinimumSize(new java.awt.Dimension(60, 60));
+        btn6.setPreferredSize(new java.awt.Dimension(60, 60));
+
+        btn9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
+        btn9.setMaximumSize(new java.awt.Dimension(60, 60));
+        btn9.setMinimumSize(new java.awt.Dimension(60, 60));
+        btn9.setPreferredSize(new java.awt.Dimension(60, 60));
+
+        btn12.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
+        btn12.setMaximumSize(new java.awt.Dimension(60, 60));
+        btn12.setMinimumSize(new java.awt.Dimension(60, 60));
+        btn12.setPreferredSize(new java.awt.Dimension(60, 60));
+
+        btn11.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
+        btn11.setMaximumSize(new java.awt.Dimension(60, 60));
+        btn11.setMinimumSize(new java.awt.Dimension(60, 60));
+        btn11.setPreferredSize(new java.awt.Dimension(60, 60));
+
+        btn10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
+        btn10.setMaximumSize(new java.awt.Dimension(60, 60));
+        btn10.setMinimumSize(new java.awt.Dimension(60, 60));
+        btn10.setPreferredSize(new java.awt.Dimension(60, 60));
+
+        btn13.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
+        btn13.setMaximumSize(new java.awt.Dimension(60, 60));
+        btn13.setMinimumSize(new java.awt.Dimension(60, 60));
+        btn13.setPreferredSize(new java.awt.Dimension(60, 60));
+
+        btn16.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
+        btn16.setMaximumSize(new java.awt.Dimension(60, 60));
+        btn16.setMinimumSize(new java.awt.Dimension(60, 60));
+        btn16.setPreferredSize(new java.awt.Dimension(60, 60));
+
+        btn15.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
+        btn15.setMaximumSize(new java.awt.Dimension(60, 60));
+        btn15.setMinimumSize(new java.awt.Dimension(60, 60));
+        btn15.setPreferredSize(new java.awt.Dimension(60, 60));
+        btn15.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn15ActionPerformed(evt);
+            }
+        });
+
+        btn14.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
+        btn14.setMaximumSize(new java.awt.Dimension(60, 60));
+        btn14.setMinimumSize(new java.awt.Dimension(60, 60));
+        btn14.setPreferredSize(new java.awt.Dimension(60, 60));
+
+        jButton17.setText("Reiniciar");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(49, 49, 49)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jButton17)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(btn13, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(btn14, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(btn15, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(btn16, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(btn9, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(btn10, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(btn11, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(btn12, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btn5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(btn6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(btn7, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(btn8, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btn1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(Titulo)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(btn2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(btn3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(btn4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))))
+                .addContainerGap(63, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addComponent(Titulo)
+                .addGap(49, 49, 49)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btn1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btn5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn7, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn8, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btn9, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn10, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn11, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn12, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btn13, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn14, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn15, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn16, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 84, Short.MAX_VALUE)
+                .addComponent(jButton17)
+                .addGap(33, 33, 33))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btn3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btn3ActionPerformed
+
+    private void btn15ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn15ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btn15ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
+            logger.log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> new PanelMain().setVisible(true));
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Titulo;
+    private javax.swing.JButton btn1;
+    private javax.swing.JButton btn10;
+    private javax.swing.JButton btn11;
+    private javax.swing.JButton btn12;
+    private javax.swing.JButton btn13;
+    private javax.swing.JButton btn14;
+    private javax.swing.JButton btn15;
+    private javax.swing.JButton btn16;
+    private javax.swing.JButton btn2;
+    private javax.swing.JButton btn3;
+    private javax.swing.JButton btn4;
+    private javax.swing.JButton btn5;
+    private javax.swing.JButton btn6;
+    private javax.swing.JButton btn7;
+    private javax.swing.JButton btn8;
+    private javax.swing.JButton btn9;
+    private javax.swing.JButton jButton17;
+    private javax.swing.JLabel jLabel1;
+    // End of variables declaration//GEN-END:variables
+}
