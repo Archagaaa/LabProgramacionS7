2ccc1e320a2f6c15499d2609fb62de4c472f7044
Aaron Archaga <Aaron Archaga@Laptop_Archaga>
2025-06-06 09:52:49 -0600

    commit de prueba

--- src/main/java/Metodos/Logica.java
+++ src/main/java/Metodos/Logica.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Metodos;
 
-/**
- *
- * @author adrianaguilar
- */
+import java.util.Random;
+
 public class Logica {
     
+    //Metodo para asignar los numeros a las cartas
+    
+   public int []getNumCartas(){  
+
+        int[] numeros = new int[16];
+        int count = 0; 
+
+        while(count<16){
+            Random r = new Random(); 
+            int na = r.nextInt(8)+1;   
+            int nvr =0; 
+
+                for (int i = 0; i < 16; i++) {
+                    if(numeros[i]==na){
+                    nvr++; 
+                    }
+                }
+            if(nvr<2){
+            numeros[count]=na;
+            count++;
+            }
+        }
+       return numeros; 
+    }
 }
--- src/main/java/Metodos/PanelMain.java
+++ src/main/java/Metodos/PanelMain.java
@@ -1,14 +1,71 @@
 package Metodos;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
 public class PanelMain extends javax.swing.JFrame {
     
-    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(PanelMain.class.getName());
-
-   
+    boolean caraarriba = false;
+    private Logica log = new Logica();
+    private boolean caraUp = false;
+    private ImageIcon Im1, Im2;
+    private JButton[] pbtn = new JButton[2];
+    private boolean primerc = false;
+    
     public PanelMain() {
         initComponents();
     }
 
+    private void setCards() {      
+        int[] numbers = log.getNumCartas();  
+        int[][] matriz = arreglobidimensional(numbers);
+
+        //Matriz de los botones
+        JButton[][] botones = {
+            {btn1, btn2, btn3, btn4},
+            {btn5, btn6, btn7, btn8},
+            {btn9, btn10, btn11, btn12},
+            {btn13, btn14, btn15, btn16}
+        };
+
+        //Asigna las imagenes a cada boton de la matriz
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                botones[i][j].setDisabledIcon(new ImageIcon(getClass().getResource("/imagenes/Img" + matriz[i][j] + ".png")));
+            }
+        }
+    }
+    
+    //El metodo convierte el arreglo unidimensional al bidimensional a una matriz 4x4
+    private int[][] arreglobidimensional(int[] numbers) {
+        int[][] matriz = new int[4][4];
+        int indice = 0;
+
+        //Recorre la matriz
+        for (int filas = 0; filas < 4; filas++) {
+            for (int columnas = 0; columnas < 4; columnas++) {
+                matriz[filas][columnas] = numbers[indice++];
+            }
+        }
+        return matriz;
+    }
+    
+    private void btnEnabled(JButton btn) {
+
+        if (!caraUp) {                 //si caraUp=fasle no hay cartas seleccionadas
+            btn.setEnabled(false);       //desabilita el boton de la carta de seleccionada
+            Im1 = (ImageIcon) btn.getDisabledIcon();  //guarda la imagen de la carta en una variable
+            pbtn[0] = btn;                 // guarda el boton en variable 
+            caraUp = true;                 // caraUp=true indica la primera carta fue seleccionada
+            primerc = false;              // primero=false indica segunda carta no seleccionada
+
+        } else {
+            btn.setEnabled(false);       
+            pbtn[1] = btn;
+            Im2 = (ImageIcon) btn.getDisabledIcon();
+            primerc = true;
+        }
+    }
     
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -45,11 +102,21 @@ public class PanelMain extends javax.swing.JFrame {
         btn2.setMaximumSize(new java.awt.Dimension(60, 60));
         btn2.setMinimumSize(new java.awt.Dimension(60, 60));
         btn2.setPreferredSize(new java.awt.Dimension(60, 60));
+        btn2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn2ActionPerformed(evt);
+            }
+        });
 
         btn1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
         btn1.setMaximumSize(new java.awt.Dimension(60, 60));
         btn1.setMinimumSize(new java.awt.Dimension(60, 60));
         btn1.setPreferredSize(new java.awt.Dimension(60, 60));
+        btn1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn1ActionPerformed(evt);
+            }
+        });
 
         btn3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
         btn3.setMaximumSize(new java.awt.Dimension(60, 60));
@@ -65,56 +132,111 @@ public class PanelMain extends javax.swing.JFrame {
         btn4.setMaximumSize(new java.awt.Dimension(60, 60));
         btn4.setMinimumSize(new java.awt.Dimension(60, 60));
         btn4.setPreferredSize(new java.awt.Dimension(60, 60));
+        btn4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn4ActionPerformed(evt);
+            }
+        });
 
         btn5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
         btn5.setMaximumSize(new java.awt.Dimension(60, 60));
         btn5.setMinimumSize(new java.awt.Dimension(60, 60));
         btn5.setPreferredSize(new java.awt.Dimension(60, 60));
+        btn5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn5ActionPerformed(evt);
+            }
+        });
 
         btn8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
         btn8.setMaximumSize(new java.awt.Dimension(60, 60));
         btn8.setMinimumSize(new java.awt.Dimension(60, 60));
         btn8.setPreferredSize(new java.awt.Dimension(60, 60));
+        btn8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn8ActionPerformed(evt);
+            }
+        });
 
         btn7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
         btn7.setMaximumSize(new java.awt.Dimension(60, 60));
         btn7.setMinimumSize(new java.awt.Dimension(60, 60));
         btn7.setPreferredSize(new java.awt.Dimension(60, 60));
+        btn7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn7ActionPerformed(evt);
+            }
+        });
 
         btn6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
         btn6.setMaximumSize(new java.awt.Dimension(60, 60));
         btn6.setMinimumSize(new java.awt.Dimension(60, 60));
         btn6.setPreferredSize(new java.awt.Dimension(60, 60));
+        btn6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn6ActionPerformed(evt);
+            }
+        });
 
         btn9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
         btn9.setMaximumSize(new java.awt.Dimension(60, 60));
         btn9.setMinimumSize(new java.awt.Dimension(60, 60));
         btn9.setPreferredSize(new java.awt.Dimension(60, 60));
+        btn9.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn9ActionPerformed(evt);
+            }
+        });
 
         btn12.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
         btn12.setMaximumSize(new java.awt.Dimension(60, 60));
         btn12.setMinimumSize(new java.awt.Dimension(60, 60));
         btn12.setPreferredSize(new java.awt.Dimension(60, 60));
+        btn12.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn12ActionPerformed(evt);
+            }
+        });
 
         btn11.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
         btn11.setMaximumSize(new java.awt.Dimension(60, 60));
         btn11.setMinimumSize(new java.awt.Dimension(60, 60));
         btn11.setPreferredSize(new java.awt.Dimension(60, 60));
+        btn11.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn11ActionPerformed(evt);
+            }
+        });
 
         btn10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
         btn10.setMaximumSize(new java.awt.Dimension(60, 60));
         btn10.setMinimumSize(new java.awt.Dimension(60, 60));
         btn10.setPreferredSize(new java.awt.Dimension(60, 60));
+        btn10.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn10ActionPerformed(evt);
+            }
+        });
 
         btn13.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
         btn13.setMaximumSize(new java.awt.Dimension(60, 60));
         btn13.setMinimumSize(new java.awt.Dimension(60, 60));
         btn13.setPreferredSize(new java.awt.Dimension(60, 60));
+        btn13.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn13ActionPerformed(evt);
+            }
+        });
 
         btn16.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
         btn16.setMaximumSize(new java.awt.Dimension(60, 60));
         btn16.setMinimumSize(new java.awt.Dimension(60, 60));
         btn16.setPreferredSize(new java.awt.Dimension(60, 60));
+        btn16.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn16ActionPerformed(evt);
+            }
+        });
 
         btn15.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/interrogar.png"))); // NOI18N
         btn15.setMaximumSize(new java.awt.Dimension(60, 60));
@@ -130,6 +252,11 @@ public class PanelMain extends javax.swing.JFrame {
         btn14.setMaximumSize(new java.awt.Dimension(60, 60));
         btn14.setMinimumSize(new java.awt.Dimension(60, 60));
         btn14.setPreferredSize(new java.awt.Dimension(60, 60));
+        btn14.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn14ActionPerformed(evt);
+            }
+        });
 
         jButton17.setText("Reiniciar");
 
@@ -219,11 +346,83 @@ public class PanelMain extends javax.swing.JFrame {
 
     private void btn3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn3ActionPerformed
         // TODO add your handling code here:
+        btnEnabled(btn3);
     }//GEN-LAST:event_btn3ActionPerformed
 
     private void btn15ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn15ActionPerformed
         // TODO add your handling code here:
+        btnEnabled(btn15);
     }//GEN-LAST:event_btn15ActionPerformed
+
+    private void btn1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn1ActionPerformed
+        // TODO add your handling code here:
+        btnEnabled(btn1);
+    }//GEN-LAST:event_btn1ActionPerformed
+
+    private void btn2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn2ActionPerformed
+        // TODO add your handling code here:
+        btnEnabled(btn2);
+    }//GEN-LAST:event_btn2ActionPerformed
+
+    private void btn4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn4ActionPerformed
+        // TODO add your handling code here:
+        btnEnabled(btn4);
+    }//GEN-LAST:event_btn4ActionPerformed
+
+    private void btn5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn5ActionPerformed
+        // TODO add your handling code here
+        btnEnabled(btn5);
+    }//GEN-LAST:event_btn5ActionPerformed
+
+    private void btn6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn6ActionPerformed
+        // TODO add your handling code here:
+        btnEnabled(btn6);
+    }//GEN-LAST:event_btn6ActionPerformed
+
+    private void btn7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn7ActionPerformed
+        // TODO add your handling code here:
+        btnEnabled(btn7);
+    }//GEN-LAST:event_btn7ActionPerformed
+
+    private void btn8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn8ActionPerformed
+        // TODO add your handling code here:
+        btnEnabled(btn8);
+    }//GEN-LAST:event_btn8ActionPerformed
+
+    private void btn9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn9ActionPerformed
+        // TODO add your handling code here:
+        btnEnabled(btn9);
+    }//GEN-LAST:event_btn9ActionPerformed
+
+    private void btn10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn10ActionPerformed
+        // TODO add your handling code here:
+        btnEnabled(btn10);
+    }//GEN-LAST:event_btn10ActionPerformed
+
+    private void btn11ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn11ActionPerformed
+        // TODO add your handling code here:
+        btnEnabled(btn11);
+    }//GEN-LAST:event_btn11ActionPerformed
+
+    private void btn12ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn12ActionPerformed
+        // TODO add your handling code here:
+        btnEnabled(btn12);
+    }//GEN-LAST:event_btn12ActionPerformed
+
+    private void btn13ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn13ActionPerformed
+        // TODO add your handling code here:
+        btnEnabled(btn13);
+    }//GEN-LAST:event_btn13ActionPerformed
+
+    private void btn14ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn14ActionPerformed
+        // TODO add your handling code here:
+        btnEnabled(btn14);
+    }//GEN-LAST:event_btn14ActionPerformed
+
+    private void btn16ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn16ActionPerformed
+        // TODO add your handling code here:
+        btnEnabled(btn16);
+    }//GEN-LAST:event_btn16ActionPerformed
 
     /**
      * @param args the command line arguments
